591cd75720a47329ce1068bc1f315c24561f9c0e
ohin6 <owen.williams-4@postgrad.manchester.ac.uk>
2021-05-12 11:00:21 +0100

    upload project

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace FilterData
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Txt_Search = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Btn_Open = new System.Windows.Forms.Button();
+            this.Txt_Records = new System.Windows.Forms.TextBox();
+            this.Lbl_Total = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Location = new System.Drawing.Point(688, 43);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(84, 29);
+            this.btn_Search.TabIndex = 0;
+            this.btn_Search.Text = "Search";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            this.btn_Search.Click += new System.EventHandler(this.btn_Search_Click);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(12, 43);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(48, 15);
+            this.Label1.TabIndex = 1;
+            this.Label1.Text = "Search :";
+            // 
+            // Txt_Search
+            // 
+            this.Txt_Search.Location = new System.Drawing.Point(66, 46);
+            this.Txt_Search.Name = "Txt_Search";
+            this.Txt_Search.Size = new System.Drawing.Size(614, 23);
+            this.Txt_Search.TabIndex = 2;
+            this.Txt_Search.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Txt_Search_KeyPress);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 89);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(745, 328);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Btn_Open
+            // 
+            this.Btn_Open.Location = new System.Drawing.Point(683, 423);
+            this.Btn_Open.Name = "Btn_Open";
+            this.Btn_Open.Size = new System.Drawing.Size(84, 29);
+            this.Btn_Open.TabIndex = 4;
+            this.Btn_Open.Text = "Open";
+            this.Btn_Open.UseVisualStyleBackColor = true;
+            this.Btn_Open.Click += new System.EventHandler(this.Btn_Open_Click);
+            // 
+            // Txt_Records
+            // 
+            this.Txt_Records.Location = new System.Drawing.Point(436, 423);
+            this.Txt_Records.Name = "Txt_Records";
+            this.Txt_Records.Size = new System.Drawing.Size(241, 23);
+            this.Txt_Records.TabIndex = 5;
+            // 
+            // Lbl_Total
+            // 
+            this.Lbl_Total.AutoSize = true;
+            this.Lbl_Total.Location = new System.Drawing.Point(22, 426);
+            this.Lbl_Total.Name = "Lbl_Total";
+            this.Lbl_Total.Size = new System.Drawing.Size(80, 15);
+            this.Lbl_Total.TabIndex = 6;
+            this.Lbl_Total.Text = "Total Records:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 461);
+            this.Controls.Add(this.Lbl_Total);
+            this.Controls.Add(this.Txt_Records);
+            this.Controls.Add(this.Btn_Open);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Txt_Search);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.btn_Search);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.TextBox Txt_Search;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Btn_Open;
+        private System.Windows.Forms.TextBox Txt_Records;
+        private System.Windows.Forms.Label Lbl_Total;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FilterData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Tutorial on https://www.youtube.com/watch?v=k44-N4Pegag&t=219s
+        private void Btn_Open_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Excel WorkBook|*.xlsx", Multiselect = false })
+            {
+                if (ofd.ShowDialog()==DialogResult.OK)
+                {
+                    //Create a DataTable from file selected in OpenFileDialog
+                    Cursor.Current = Cursors.WaitCursor;
+                    DataTable dt = new DataTable();
+                    using(XLWorkbook workbook = new XLWorkbook(ofd.FileName))//using XMLworkbook from the selected file from openfiledialog
+                    {
+                        bool isFirstRow = true; //first row added as Header
+                        var rows = workbook.Worksheet(1).RowsUsed();
+                        foreach(var row in rows)
+                        {
+                            if (isFirstRow)
+                            {
+                                //Adding Column
+                                foreach (IXLCell cell in row.Cells())
+                                    dt.Columns.Add(cell.Value.ToString());
+                                isFirstRow = false;
+
+                            }
+                            else
+                            {
+                                //adding data
+                                dt.Rows.Add();
+                                int i = 0;
+                                foreach (IXLCell cell in row.Cells())
+                                    dt.Rows[dt.Rows.Count - 1][i++] = cell.Value.ToString();
+
+                            }
+                        }
+                        //convert datatable to datagridview viewer
+                        dataGridView1.DataSource = dt.DefaultView;
+                        Lbl_Total.Text = $"Total Records: {dataGridView1.RowCount}"; //change lbl.total to display number of rows in datagrid view
+                        Cursor.Current = Cursors.Default;
+
+                    }
+                }
+            }
+        }
+
+       //allow user defined search terms on dataTable
+        private void btn_Search_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataView dv = dataGridView1.DataSource as DataView; //datatable in datagridview as variable Dataview (allows changes to datatable without affecting original file)
+                if (dv != null)
+                    dv.RowFilter = Txt_Search.Text;//RowFilter allows query of dataview table from textbox search input (Txt_search)
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);//error popup icon
+            }
+        }
+
+        private void Txt_Search_KeyPress(object sender, KeyPressEventArgs e) //called this function by going into From1.cs{design} select textbox-> actions ->keypress
+        {
+            if (e.KeyChar == (char)13)// limiting search terms to 13 characters?
+                btn_Search.PerformClick();
+        }
+    }
+}
